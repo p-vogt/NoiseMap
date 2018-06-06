c92f45505a429c4311a39f38197a7fdaea407f4e
Unknown <patrick.vogt@fh-bielefeld.de>
2018-06-06 14:51:25 +0200

    fix json parse for the new sample request

--- NoiseRecorder/app/src/main/java/com/example/patrick/noiserecorder/Location/LocationTrackerBroadcastReceiver.java
+++ NoiseRecorder/app/src/main/java/com/example/patrick/noiserecorder/Location/LocationTrackerBroadcastReceiver.java
@@ -40,23 +40,46 @@ public class LocationTrackerBroadcastReceiver extends BroadcastReceiver {
         Calendar calendar = Calendar.getInstance();
         SimpleDateFormat timestampFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
         String timestamp = timestampFormat.format(calendar.getTime());
+        double speed = -1.0d;
+        double longitude = -1.0d;
+        double latitude = -1.0d;
+        double accuracy = -1.0d;
+        try {
+            longitude = message.getDouble("longitude");
+        } catch (JSONException e) { }
+        try {
+            latitude = message.getDouble("latitude");
+        } catch (JSONException e) { }
+        try {
+            accuracy = message.getDouble("accuracy");
+        } catch (JSONException e) { }
+        try {
+            speed = message.getDouble("speed");
+        } catch (JSONException e) { }
 
+        if(speed == -1.0d ||longitude == -1.0d ||latitude == -1.0d ||accuracy == -1.0d ) {
+            int i = 0;
+        }
         JSONObject jsonBody = new JSONObject();
         try {
+            double lastAverageDb = this.recording.getLastAverageDb();
+            if(Double.isNaN(lastAverageDb)) {
+                lastAverageDb = -1.0d;
+            }
             jsonBody.put("timestamp", timestamp);
-            jsonBody.put("noiseValue", this.recording.getLastAverageDb());
-            jsonBody.put("longitude", message.getDouble("longitude"));
-            jsonBody.put("latitude", message.getDouble("latitude"));
-            jsonBody.put("accuracy", message.getDouble("accuracy"));
-            jsonBody.put("speed", message.getDouble("speed"));
+            jsonBody.put("noiseValue", lastAverageDb);
+            jsonBody.put("longitude", longitude);
+            jsonBody.put("latitude",latitude);
+            jsonBody.put("accuracy", accuracy);
+            jsonBody.put("speed", speed);
             jsonBody.put("version", "AAAAAAAAB9g="); // TODO ??
             jsonBody.put("createdAt", timestamp);
             jsonBody.put("updatedAt", timestamp);
             jsonBody.put("deleted", false);
 
         } catch (JSONException e) {
-            e.printStackTrace(); // TODO
-            return new JSONObject();
+
+            return new JSONObject(); //TODO
         }
         return jsonBody;
     }
