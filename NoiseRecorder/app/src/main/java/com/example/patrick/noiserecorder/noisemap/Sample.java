30ebc3ae59cb9d76d49ef52513a6d7f8eb9dcc77
Unknown <patrick.vogt@fh-bielefeld.de>
2018-07-01 15:37:31 +0200

    add timestamp to Sample class

--- NoiseRecorder/app/src/main/java/com/example/patrick/noiserecorder/noisemap/Sample.java
+++ NoiseRecorder/app/src/main/java/com/example/patrick/noiserecorder/noisemap/Sample.java
@@ -1,15 +1,18 @@
 package com.example.patrick.noiserecorder.noisemap;
 
 import com.google.android.gms.maps.model.LatLng;
+import java.util.Date;
 
 //TODO
 public class Sample {
     public LatLng position;
     public double noise;
+    public Date timestamp;
 
-    public Sample(LatLng position, double noise) {
+    public Sample(LatLng position, double noise, Date timestamp) {
         this.position = position;
         this.noise = noise;
+        this.timestamp = timestamp;
 
     }
 }
