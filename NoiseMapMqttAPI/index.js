415f1a3c17e2f88ece19b2598376a4c3885b852a
Unknown <patrick.vogt@fh-bielefeld.de>
2018-07-07 22:08:10 +0200

    add basic mqtt logic

--- NoiseMapMqttAPI/index.js
+++ NoiseMapMqttAPI/index.js
@@ -1,18 +1,33 @@
-var mosca = require('mosca');
+const mosca = require('mosca');
+const mqtt = require('mqtt');
 
-var settings = {
+const settings = {
   port: 1883
 };
 
-var server = new mosca.Server(settings);
+const server = new mosca.Server(settings);
+const mqttClient = mqtt.connect('tcp://127.0.0.1:1883', { clientId: '0' });
 
-server.on('clientConnected', function(client) {
+mqttClient.on('message', function (topic, message) {
+    if(topic.includes("request")) {
+      mqttClient.publish(topic.replace("request","response"),"moinsen");
+    }
+})
+
+mqttClient.on('connect', function () {
+
+})
+
+server.on('clientConnected', function (client) {
+  if (client.id != '0') {
+    mqttClient.subscribe("clients/+/request");
     console.log('client connected', client.id);
+  }
 });
 
 // fired when a message is received
-server.on('published', function(packet, client) {
-  console.log('Published', packet.payload);
+server.on('published', function (packet, client) {
+  console.log('Published', packet);
 });
 
 server.on('ready', setup);
